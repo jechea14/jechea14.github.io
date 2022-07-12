cd7be3ae45f0bc8dbedf9e9a0a8a46e0dfc5171a
Jeanie Chea <chea.jeanie@gmail.com>
2022-07-12 10:55:12 -0700

    React + Tailwind

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="dark:bg-slate-800 h-screen">
+      hello world
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.{html,js}"],
+  // content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
