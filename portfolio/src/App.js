58dce8b0caee2b56d060e565fb0d19bbd0024026
Jeanie Chea <chea.jeanie@gmail.com>
2022-07-21 02:22:01 -0700

    Transition to mobile first approach

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,7 +8,7 @@ import Contact from './Components/Contact';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center">
+    <div className="min-h-screen flex flex-col items-center px-5">
       <div>
         <NavBar/>
         <Intro/>
