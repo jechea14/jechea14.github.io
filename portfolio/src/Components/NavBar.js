63eab691f852364a35ba10bbc06baa3cfbe50804
Jeanie Chea <chea.jeanie@gmail.com>
2022-07-22 01:36:59 -0700

    Mobile navbar

--- portfolio/src/Components/NavBar.js
+++ portfolio/src/Components/NavBar.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react'
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 const NavBar = () => {
 
-  const [isShown, setIsShown] = useState(false)
+  const [isMobileNavShown, setIsMobileNavShown] = useState(false)
+  const handleClick = () => setIsMobileNavShown(!isMobileNavShown)
 
   return (
-    <nav className='flex justify-between py-6 items-center'>
+    <nav className='flex justify-between py-6 items-center w-full'>
       <div className='text-xl font-bold'>
         <h1>Jeanie Chea</h1>
       </div>
+
+      {/* mobile menu */}
       <div>
-        <ul className='flex flex-col text-slate-600'>
+        <ul className={!isMobileNavShown ? 'hidden' : 'absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center text-slate-600 bg-slate-300'}>
+          <a href='#home' className='hover:text-slate-900 transition py-6 text-4xl'>Home</a>
+          <a href='#about-me' className='hover:text-slate-900 transition py-6 text-4xl'>About Me</a>
+          <a href='#work' className='hover:text-slate-900 transition py-6 text-4xl'>Work</a>
+          <a href='#contact' className='hover:text-slate-900 transition py-6 text-4xl'>Contact</a>
+        </ul>
+      </div>
+
+      {/* desktop menu */}
+      <div>
+        <ul className='md:flex space-x-8 text-slate-600 hidden'>
           <a href='#home' className='hover:text-slate-900 transition'>Home</a>
           <a href='#about-me' className='hover:text-slate-900 transition'>About Me</a>
           <a href='#work' className='hover:text-slate-900 transition'>Work</a>
@@ -19,19 +32,10 @@ const NavBar = () => {
         </ul>
       </div>
 
-      <div>
-        <FaBars/>
+      <div onClick={handleClick} className='md:hidden z-10 cursor-pointer'>
+        {!isMobileNavShown ? <FaBars/> : <FaTimes/>}
       </div>
     </nav>
-    // <nav className='flex justify-between py-6 items-center'>
-    //   <h1 className='text-xl font-bold'>Jeanie Chea</h1>
-    //   <ul className='flex space-x-6 text-slate-600'>
-    //     <a href='#home' className='hover:text-slate-900 transition'>Home</a>
-    //     <a href='#about-me' className='hover:text-slate-900 transition'>About Me</a>
-    //     <a href='#work' className='hover:text-slate-900 transition'>Work</a>
-    //     <a href='#contact' className='hover:text-slate-900 transition'>Contact</a>
-    //   </ul>
-    // </nav>
   )
 }
 
