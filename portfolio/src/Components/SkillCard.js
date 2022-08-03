b05b743068f3b2342fdb3e329a60bd2d06d8ede7
Jeanie Chea <chea.jeanie@gmail.com>
2022-08-03 02:26:19 -0700

    Working mobile navbar, refractored duplicate code into map functions

--- portfolio/src/Components/SkillCard.js
+++ portfolio/src/Components/SkillCard.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const SkillCard = ({icon, name}) => {
   return (
-    <div className='border rounded-xl bg-gray-100 shadow-md'>
+    <div className='border rounded-xl bg-gray-50 shadow-md'>
       <div className='flex flex-col justify-center items-center m-4'>
         <div className='mb-2'>
           {icon}
         </div>
-        <p className='text-xs'>{name}</p>
+        <p className='text-xs font-bold'>{name}</p>
       </div>
     </div>
   )
--- portfolio/src/Components/Skills.js
+++ portfolio/src/Components/Skills.js
@@ -3,18 +3,49 @@ import SkillCard from './SkillCard'
 import { FaHtml5, FaCss3Alt, FaPython, FaReact } from 'react-icons/fa'
 import { SiJavascript, SiTailwindcss } from 'react-icons/si'
 
+const skills = [
+  {
+    id: 1,
+    icon: <FaHtml5 size={50} color={'orange'}/>,
+    name: 'HTML5'
+  },
+  {
+    id: 2,
+    icon: <FaCss3Alt size={50} color={'blue'}/>,
+    name: 'CSS3'
+  },
+  {
+    id: 3,
+    icon: <SiJavascript size={50}/>,
+    name: 'JavaScript'
+  },
+  {
+    id: 4,
+    icon: <FaReact size={50} color={'lightblue'}/>,
+    name: 'ReactJS'
+  },
+  {
+    id: 5,
+    icon: <SiTailwindcss size={50} color={'lightblue'}/>,
+    name: 'TailwindCSS'
+  },
+  {
+    id: 6,
+    icon: <FaPython size={50}/>,
+    name: 'Python3'
+  },
+]
+
 const Skills = () => {
   return (
     <div>
-      <h1 className='text-2xl font-bold text-left pt-10'>Skills</h1>
-      <div className='grid grid-cols-wrap gap-6 border rounded-xl py-6'>
-        <SkillCard icon={<FaHtml5 size={50} color={'orange'}/>} name={'HTML5'}/>
-        <SkillCard icon={<FaCss3Alt size={50} color={'blue'}/>} name={'CSS3'}/>
-        <SkillCard icon={<SiJavascript size={50}/>} name={'JavaScript'}/>
-        <SkillCard icon={<FaReact size={50} color={'lightblue'}/>} name={'ReactJS'}/>
-        <SkillCard icon={<SiTailwindcss size={50} color={'lightblue'}/>} name={'TailwindCSS'}/>
-        <SkillCard icon={<FaPython size={50}/>} name={'Python3'}/>
-        
+      <h1 className='text-xl font-bold text-left pt-10'>SKILLS</h1>
+      <div className='grid grid-cols-wrap gap-6 rounded-xl py-6'>
+        {
+          skills.map((skill) => (
+            <SkillCard key={skill.id} icon={skill.icon} name={skill.name}/>
+          ))
+        }
       </div>
     </div>
   )
