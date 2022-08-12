a9ade3f63bacc62b6c2f69e75a1a96f1f0be3a21
Jeanie Chea <chea.jeanie@gmail.com>
2022-08-12 01:32:12 -0700

    Add more skills and projects

--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ContactForm from './ContactForm'
 
 const Contact = () => {
   return (
     <div id='contact' className='scroll-mt-10'>
       <h1 className='text-2xl font-bold text-left pt-10'>CONTACT</h1>
+      <ContactForm/>
     </div>
   )
 }
