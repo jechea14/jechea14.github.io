d6f3c20c402ddb19a88052be694133dbf2c94aef
Jeanie Chea <chea.jeanie@gmail.com>
2022-08-20 12:54:01 -0700

    Add functional contact form, add more contact details

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import NavBar from './Components/NavBar';
 import Intro from './Components/Intro';
 import AboutMe from './Components/AboutMe';
--- portfolio/src/Components/ContactForm.js
+++ portfolio/src/Components/ContactForm.js
@@ -1,23 +1,36 @@
 import React from 'react'
+import * as emailjs from 'emailjs-com';
 
 const ContactForm = () => {
+
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_hgfd3yl', 'template_m5izejp', e.target, 'IsKftCyPiMttClna1')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  }
+
   return (
-    <div>
-      <form className=' border rounded-xl p-10 bg-gray-50 shadow-md'>
-      WORK IN PROGRESS
-        <div className='flex flex-col justify-center'>
-          <label for='name'>Name:</label>
-          <input type='text' name='name' className='border border-gray-300 rounded-md px-2' required/>
+    <div className='md:flex-auto lg:basis-1/2'>
+      <h2 className='text-lg font-semibold'>Contact Form</h2>
+      <form onSubmit={sendEmail} className=' border rounded-md p-8 bg-gray-50 shadow-md'>
+        <div className='flex flex-col justify-center space-y-5'>
+          <input type='text' name='name' placeholder='Name*' className='border border-gray-300 rounded-md px-4 py-2' required/>
 
-          <label for='email'>Email:</label>
-          <input type='email' name='email' className='border border-gray-300 rounded-md px-2' required/>
+          <input type='email' name='email' placeholder='Email*' className='border border-gray-300 rounded-md px-4 py-2' required/>
 
-          <label for='message'>Message:</label>
-          <textarea name='message' rows='10' className='border border-gray-300 rounded-md px-2' required/>
+          <input type='text' name='subject' placeholder='Subject*' className='border border-gray-300 rounded-md px-4 py-2' required/>
 
+          <textarea name='message' rows='5' placeholder='Message*' className='border border-gray-300 rounded-md px-4 py-2' required/>
         </div>
 
-        <input type='submit' value='Submit' className='bg-purple-600 text-gray-50 px-6 py-2 rounded-xl cursor-pointer hover:bg-purple-800 transition'/>
+        <input type='submit' value='Send' className='mt-4 bg-purple-600 text-gray-50 px-6 py-2 rounded-xl cursor-pointer hover:bg-purple-800 transition'/>
       </form>
     </div>
   )
