5bf920706fa6dd430dc0baeb8f57c2bf03ae013a
Jeanie Chea <chea.jeanie@gmail.com>
2023-05-07 01:36:00 -0700

    Change project card layout on large screens

--- portfolio/src/Components/ProjectCard.js
+++ portfolio/src/Components/ProjectCard.js
@@ -10,32 +10,38 @@ const ProjectCard = ({
   linkCode,
 }) => {
   return (
-    <div className="flex flex-col justify-between border rounded-xl text-center bg-gray-50 shadow-md hover:shadow-xl hover:transition dark:bg-slate-700 dark:border-slate-700">
-      <div>
+    <div className="flex flex-col justify-between rounded-xl text-center bg-gray-50 shadow-md hover:shadow-xl hover:transition dark:bg-slate-700 dark:border-slate-700 lg:flex-row">
+      <div className="lg:max-h-fit lg:max-w-xl overflow-hidden">
         {/* project picture */}
-        <picture className="">
-          <img
-            src={image}
-            alt={image}
-            loading="lazy"
-            className="rounded-tl-lg rounded-tr-lg"
-          />
-        </picture>
+        <img
+          src={image}
+          alt={image}
+          loading="lazy"
+          className="rounded-tl-xl rounded-tr-xl lg:rounded-tr-none lg:rounded-bl-xl"
+
+        />
+
+      </div>
+
+      {/* Demo and Code buttons */}
+      <div className="mx-4 flex flex-col justify-between">
         <div className="pt-4 px-4">
           {/* project title */}
           <h1 className="font-semibold text-lg pt-2">{title}</h1>
           {/* technologies used */}
           <p className="text-xs mb-2">{techUsed}</p>
           {/* project description */}
-          <p className="text-left pt-2 border-t-2 pb-2 min-h-full">
-            {description}
+          <p className="text-left pt-2 border-t-2 pb-2">
+            {description.map((desc) => {
+              return (
+                <li className="list-disc">
+                  {desc}
+                </li>
+              )
+            })}
           </p>
         </div>
-      </div>
-
-      {/* Demo and Code buttons */}
-      <div className="border-t-2 mx-4">
-        <div className="flex justify-evenly py-4">
+        <div className="flex justify-evenly py-14">
           <Button name={"Demo"} link={linkDemo} />
           <Button name={"Code"} link={linkCode} />
         </div>
